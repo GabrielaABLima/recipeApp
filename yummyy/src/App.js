784e785700c05ad6169a4349c1cd947732b8c554
GabrielaABLima <gabriela.andrade.bueno.lima@gmail.com>
2023-02-22 22:28:41 -0300

    research field

--- yummyy/src/App.js
+++ yummyy/src/App.js
@@ -2,11 +2,13 @@ import Pages from "./pages/Pages";
 import React from "react";
 import Categorie from "./components/Categorie";
 import {BrowserRouter} from 'react-router-dom'
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search/>
         <Categorie/> 
         <Pages />
       </BrowserRouter>
--- yummyy/src/pages/Pages.jsx
+++ yummyy/src/pages/Pages.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './Home'
 import {Route, Routes} from 'react-router-dom';
 import Cuisine from "./Cuisine"
+import Searched from './Searched';
 
 function Pages() {
   return (
@@ -10,6 +11,7 @@ function Pages() {
 
           <Route path="/" element={<Home/>} />
           <Route path="/cuisine/:type" element={<Cuisine/>} />
+          <Route path="/Searched/:search" element={<Searched/>} />
       </Routes>
     
   );
